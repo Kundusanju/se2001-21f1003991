f588bc5e3ee735388fdab9dbf855c7bedee249d3
Kundusanju <21f1003991@ds.study.iitm.ac.in>
2024-04-26 13:54:21 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="Kundusanju"
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
+export GITHUB_REPOSITORY="se2001-21f1003991"
